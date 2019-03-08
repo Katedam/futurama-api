3e1baf30d931fc0818479803470485896ff8cc55
Kate Dameron <katerj33@gmail.com>
2019-03-08 12:10:00 -0800

    scraper

---
+++ src/services/scraper.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+const request = require('superagent');
+const { parse } = require('node-html-parser');
+
+const fetchFuturamaData = () => {
+
+  return request.get('http://www.parseerror.com/pizza/futurama/')
+    .then(res => res.text)
+    .then(parse)
+    .then(findPairs)
+    .then(returnQuotes)
+    .then(console.log);
+};
+
+const findQuotes = html => html.querySelectorAll('.qt'); 
+const findCharacters = html => html.querySelectorAll('.n');
+const findPairs = html => [findQuotes(html), findCharacters(html)];
+
+const returnQuotes = ([quotes, characters])=> quotes.map((quote, i) => {
+  return {
+    character: characters[i].text.trim(),
+    quote: quote.text.trim()
+  };
+});
+
+fetchFuturamaData();
