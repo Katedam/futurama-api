0d3e972574bfddae50a5cd2fbd2e0cb745b0a4de
Kate Dameron <katerj33@gmail.com>
2019-03-08 17:18:06 -0800

    images added to data and function checking for capitalization in url request

--- seed.js
+++ seed.js
@@ -6,7 +6,33 @@ const mongoose = require('mongoose');
 // eslint-disable-next-line no-undef
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
+const benderImage = 'https://imgur.com/TQbrGRN';
+const fryImage = 'https://imgur.com/aLPxYmf';
+const leelaImage = 'https://imgur.com/uo3u9u1';
+
 fetchFuturamaData()
-  .then(quotes => Quote.create(quotes))
+  .then(quotes => {
+    return quotes.map(quote => {
+      if(quote.character === 'Bender') {
+        return {
+          ...quote,
+          image: benderImage,
+        };
+      } 
+      if(quote.character === 'Leela') {
+        return {
+          ...quote,
+          image: leelaImage,
+        };
+      }
+      if(quote.character === 'Fry') {
+        return {
+          ...quote,
+          image: fryImage,
+        };
+      }
+    });
+  })
+  .then(quotesWithImages => Quote.create(quotesWithImages))
   .finally(() => mongoose.connection.close());
 
