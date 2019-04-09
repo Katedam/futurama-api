dba3570e82cbe96c7fb7a38589618b3e84d601e6
Kate Dameron <katerj33@gmail.com>
2019-04-08 17:29:55 -0700

    fixing port

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 require('./lib/utils/connect')();
 const app = require('./lib/app');
 
-app.listen(7890, () => {
+app.listen(process.env.PORT || 7890, () => {
   console.log('running');
 });
