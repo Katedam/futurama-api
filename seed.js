1841ddc6b27d6269e0249689435c02ac9593b9bd
Kate Dameron <katerj33@gmail.com>
2019-04-09 17:08:56 -0700

    updated seed data to include images for all characters - first draft needs refactoring!

--- seed.js
+++ seed.js
@@ -9,6 +9,23 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 const benderImage = 'https://imgur.com/TQbrGRN';
 const fryImage = 'https://imgur.com/aLPxYmf';
 const leelaImage = 'https://imgur.com/uo3u9u1';
+const professorFarnsworth = 'https://imgur.com/mqtOU33';
+const morbo = 'https://imgur.com/5oYUiAM';
+const lindaTheReporter = 'https://imgur.com/eGopvZl';
+const zappBrannigan = 'https://imgur.com/woltzOI';
+const lurr = 'https://imgur.com/wymGUt1';
+const url = 'https://imgur.com/ezIkx52';
+const calculon = 'https://imgur.com/xy9m7NB';
+const robotMob = 'https://imgur.com/ErXSKFh';
+const hermes = 'https://imgur.com/4vhKA7g';
+const morganProctor = 'https://imgur.com/uILPgYJ';
+const mom = 'https://imgur.com/PrDvDyu';
+const kif = 'https://imgur.com/3IKxX9L';
+const drZoidberg = 'https://imgur.com/8jSlTzE';
+const giantBender = 'https://imgur.com/jXNROYf';
+const donBot = 'https://imgur.com/acP9TZq';
+const bobBarker = 'https://imgur.com/CMqNypr';
+const amy = 'https://imgur.com/p48918u';
 
 const replaceNewlineInQuote = quote => quote.replace(/\r?\n|\r/g, ' '); 
 
@@ -42,6 +59,143 @@ fetchFuturamaData()
         };
       }
 
+      if(quoteObj.character === 'Calculon') {
+        return {
+          ...quoteObj,
+          image: calculon,
+          quote
+        };
+      }
+
+      if(quoteObj.character === 'URL') {
+        return {
+          ...quoteObj,
+          image: url,
+          quote
+        };
+      }
+
+      if(quoteObj.character === 'Professor Farnsworth') {
+        return {
+          ...quoteObj,
+          character: 'professor-farnsworth',
+          image: professorFarnsworth,
+          quote
+        };
+      }
+      if(quoteObj.character === 'Zapp Brannigan') {
+        return {
+          ...quoteObj,
+          character: 'zapp-brannigan',
+          image: zappBrannigan,
+          quote
+        };
+      }
+      if(quoteObj.character === 'Lurr') {
+        return {
+          ...quoteObj,
+          image: lurr,
+          quote
+        };
+      }
+      if(quoteObj.character === 'Morbo') {
+        return {
+          ...quoteObj,
+          image: morbo,
+          quote
+        };
+      }
+      if(quoteObj.character === 'Linda the Reporter') {
+        return {
+          ...quoteObj,
+          character: 'linda-the-reporter',
+          image: lindaTheReporter,
+          quote
+        };
+      }
+      if(quoteObj.character === 'Dr. Zoidberg') {
+        return {
+          ...quoteObj,
+          character: 'dr-zoidberg',
+          image: drZoidberg,
+          quote
+        };
+      }
+      if(quoteObj.character === 'Hermes') {
+        return {
+          ...quoteObj,
+          image: hermes,
+          quote
+        };
+      }
+      if(quoteObj.character === 'Bob Barker') {
+        return {
+          ...quoteObj,
+          character: 'bob-barker',
+          image: bobBarker,
+          quote
+        };
+      }
+      if(quoteObj.character === 'Amy') {
+        return {
+          ...quoteObj,
+          image: amy,
+          quote
+        };
+      }
+      if(quoteObj.character === 'Morgan Proctor') {
+        return {
+          ...quoteObj,
+          character: 'morgan-proctor',
+          image: morganProctor,
+          quote
+        };
+      }
+      if(quoteObj.character === 'Don-Bot') {
+        return {
+          ...quoteObj,
+          character: 'don-bot',
+          image: donBot,
+          quote
+        };
+      }
+      if(quoteObj.character === 'Mom') {
+        return {
+          ...quoteObj,
+          image: mom,
+          quote
+        };
+      }
+      if(quoteObj.character === 'Robot Mob') {
+        return {
+          ...quoteObj,
+          character: 'robot-mob',
+          image: robotMob,
+          quote
+        };
+      }
+      if(quoteObj.character === 'Giant Bender') {
+        return {
+          ...quoteObj,
+          character: 'giant-bender',
+          image: giantBender,
+          quote
+        };
+      }
+      if(quoteObj.character === 'Kif') {
+        return {
+          ...quoteObj,
+          image: kif,
+          quote
+        };
+      }
+      if(quoteObj.character === 'Flexo') {
+        return {
+          ...quoteObj,
+          image: benderImage,
+          quote
+        };
+      }
       if(quoteObj.character) {
         return {
           ...quoteObj,
