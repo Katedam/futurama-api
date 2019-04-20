d391e3809905fd0eef42f10bc8ad718e5cac5ec8
Kate Dameron <katerj33@gmail.com>
2019-04-20 08:34:01 -0700

    updated character lis

--- seed.js
+++ seed.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const fetchFuturamaData = require('./lib/services/scraper');
 const Quote = require('./lib/Models/Quote');
 const mongoose = require('mongoose');
-const{ setImages } = require('./lib/services/prepData');
+const { setImages } = require('./lib/services/prepData');
 
 // eslint-disable-next-line no-undef
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
