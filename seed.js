93071951e3252e191c9ddaa2a20d7b8166830367
Kate Dameron <katerj33@gmail.com>
2019-05-07 14:56:22 -0700

    updated mongoose connection

--- seed.js
+++ seed.js
@@ -1,11 +1,9 @@
 require('dotenv').config();
+require('./lib/utils/connect')();
 const fetchFuturamaData = require('./lib/services/scraper');
 const Quote = require('./lib/Models/Quote');
 const mongoose = require('mongoose');
 const { setImages } = require('./lib/services/prepData');
-
-// eslint-disable-next-line no-undef
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 fetchFuturamaData()
   .then(quotes => setImages(quotes))
