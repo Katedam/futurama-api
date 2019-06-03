34c888f01bb96d670cd1bf7eb67be7a111307ee2
Kate Dameron <katerj33@gmail.com>
2019-06-02 20:37:32 -0700

    getting 1 character profile

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
 const request = require('superagent');
 const { parse } = require('node-html-parser');
+const {
+  parseAndCollectNames,
+  trimResultToJustCharacterNames,
+  getCharacterProfile,
+  findKeys,
+  findValues,
+  findName
+} = require('./lib/services/scraperHelpers');
 
-const characterNames = [];
-
-const parseAndCollectNames = res => {
-  const html = parse(res.text);
-  return html.querySelectorAll('li a').map(link => link.text);
-};
-const trimResultToJustCharacterNames = result => {
-  result
-    .slice(313, result.length - 47)
-    .forEach(characterName => characterNames.push(characterName));
-};
-const getCharacterProfile = character => {
-  return request.get(`https://futurama.fandom.com/wiki/${character}`);
-};
-const findKeys = html => {
-  return html.querySelectorAll('.pi-data-label').map(label => label.text).slice(1);
-};
-
-const findValues = html => {
-  return html.querySelectorAll('.pi-data-value').map(value => value.text).slice(1);
-};
-
-const findName = html => {
-  return html.querySelector('h1.page-header__title').text;
-};
 
 request.get('https://futurama.fandom.com/wiki/Characters')
   .then(parseAndCollectNames)
