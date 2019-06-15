206194a63291bc356ac128b9a51ac0872745ef3d
Kate Dameron <katerj33@gmail.com>
2019-06-15 09:25:59 -0700

    so broken

--- seed.js
+++ seed.js
@@ -1,15 +1,31 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
+const fs = require('fs');
 // const scrapeFuturamaQuotes = require('./lib/services/quotesScraper');
-const scrapeFuturamaCharacters = require('./lib/services/charScraper');
-// const Quote = require('./lib/Models/Quote');
-const Character = require('./lib/Models/Character');
+// const scrapeFuturamaCharacters = require('./lib/services/charScraper');
+const Quote = require('./lib/Models/Quote');
+// const Character = require('./lib/Models/Character');
 const mongoose = require('mongoose');
 // const { setImages } = require('./lib/services/prepData');
 
-scrapeFuturamaCharacters()
-  .then(chars => Character.create(chars))
+const quotes = fs.readFile('./quotesJson.js', { encoding: 'utf8' }, (err, data) => {
+  if(err) {
+    return err;
+  }
+  
+  try {
+    return data;
+  } catch(e) {
+    return e;
+  }
+});
+
+Quote.create(quotes)
   .finally(() => mongoose.connection.close());
+
+// scrapeFuturamaCharacters()
+// .then(chars => Character.create(chars))
+// .finally(() => mongoose.connection.close());
 
 // scrapeFuturamaQuotes()
 //   .then(quotes => setImages(quotes))
